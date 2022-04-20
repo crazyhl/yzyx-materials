997ba9744fd7d851814dd410612b10d86ced9933
M1racle.Hao <crazyhl@163.com>
2022-04-20 16:11:46 +0800

    add graceful shutdown

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"context"
+	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/crazyhl/yzyx-materials/internal"
 	"github.com/crazyhl/yzyx-materials/module/user"
@@ -58,13 +62,38 @@ func init() {
 
 func main() {
 	gin.SetMode(viper.GetString("RUN_MODE"))
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.POST("/user/register", user.Register)
-	r.POST("/user/login", user.Login)
-	r.Run("127.0.0.1:8080") // 监听并在 0.0.0.0:8080 上启动服务
+	router.POST("/user/register", user.Register)
+	router.POST("/user/login", user.Login)
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	go func() {
+		// 服务连接
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// 等待中断信号以优雅地关闭服务器（设置 5 秒的超时时间）
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+	log.Println("Server exiting")
+
 }
