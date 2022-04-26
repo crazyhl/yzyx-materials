f9c6c832307db6367cd0b2af9cf1040e5fd19837
M1racle.Hao <crazyhl@163.com>
2022-04-26 11:15:19 +0800

    feat base model add soft delete

--- internal/model/model.go
+++ internal/model/model.go
@@ -1,8 +1,11 @@
 package model
 
+import "gorm.io/gorm"
+
 // model 自己的 baseModel 实现
 type Model struct {
-	ID        uint  `gorm:"primarykey"`      // 自增主键
-	CreatedAt int64 `gorm:"autoCreateTime;"` // 创建时间
-	UpdatedAt int64 `gorm:"autoUpdateTime;"` // 更新时间
+	ID        uint           `gorm:"primarykey"`      // 自增主键
+	CreatedAt int64          `gorm:"autoCreateTime;"` // 创建时间
+	UpdatedAt int64          `gorm:"autoUpdateTime;"` // 更新时间
+	DeletedAt gorm.DeletedAt `gorm:"index"`           // 软删除标志
 }
