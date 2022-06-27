c1abb360422e0c5960904787829754fa312b801e
M1racle.Hao <crazyhl@163.com>
2022-06-27 16:28:13 +0800

    feat 调整品种model 索引

--- module/breed/model.go
+++ module/breed/model.go
@@ -8,15 +8,15 @@ import (
 // 品种 Model
 type Breed struct {
 	model.Model
-	UserId        uint      `gorm:"not null"` // 用户ID
+	UserId        uint      `gorm:"not null;uniqueIndex: uk_uid_code;"` // 用户ID
 	User          user.User // 账户所属用户，外键
-	Code          string    `gorm:"type:varchar(32);not null;unique"`        // 编码
-	Name          string    `gorm:"type:varchar(32);not null;unique"`        // 名称
-	NetValue      float64   `gorm:"type:decimal(20,4);not null"`             // 净值
-	Cost          float64   `gorm:"type:decimal(20,4);not null"`             // 成本
-	TotalCount    int64     `gorm:"not null;default:0;"`                     // 总份数
-	TotalCost     float64   `gorm:"type:decimal(20,4);not null; default:0;"` // 总成本
-	TotalNetValue float64   `gorm:"type:decimal(20,4);not null; default:0"`  // 总净值
+	Code          string    `gorm:"type:varchar(32);not null;uniqueIndex: uk_uid_code;"` // 编码
+	Name          string    `gorm:"type:varchar(32);not null;"`                          // 名称
+	NetValue      float64   `gorm:"type:decimal(20,4);not null"`                         // 净值
+	Cost          float64   `gorm:"type:decimal(20,4);not null"`                         // 成本
+	TotalCount    int64     `gorm:"not null;default:0;"`                                 // 总份数
+	TotalCost     float64   `gorm:"type:decimal(20,4);not null; default:0;"`             // 总成本
+	TotalNetValue float64   `gorm:"type:decimal(20,4);not null; default:0"`              // 总净值
 }
 
 func (breed *Breed) ToDto() *BreedDto {
