5e13c8461d5f7e809cb57d360d55d1045d0095df
M1racle.Hao <crazyhl@163.com>
2022-07-11 18:13:37 +0800

    feat 增加模型关联

--- module/account/dto.go
+++ module/account/dto.go
@@ -1,15 +1,19 @@
 package account
 
-import "github.com/crazyhl/yzyx-materials/internal/model"
+import (
+	"github.com/crazyhl/yzyx-materials/internal/model"
+	"github.com/crazyhl/yzyx-materials/module/breed"
+)
 
 type AccountDto struct {
 	model.Dto
-	Name               string  `json:"name"`                  // 账户名
-	Desc               string  `json:"desc"`                  // 账户描述
-	Total              float64 `json:"total"`                 // 账户总额
-	ExpectTotal        float64 `json:"expect_total"`          // 预期总投入
-	ExpectRateOfReturn uint8   `json:"expect_rate_of_return"` // 预期收益率
-	RateOfReturn       float64 `json:"rate_of_return"`        // 当前投入部分收益率
-	PerPart            float64 `json:"per_part"`              // 每份金额
-	ProfitAmount       float64 `json:"profit_amount"`         // 收益总额
+	Name               string            `json:"name"`                  // 账户名
+	Desc               string            `json:"desc"`                  // 账户描述
+	Total              float64           `json:"total"`                 // 账户总额
+	ExpectTotal        float64           `json:"expect_total"`          // 预期总投入
+	ExpectRateOfReturn uint8             `json:"expect_rate_of_return"` // 预期收益率
+	RateOfReturn       float64           `json:"rate_of_return"`        // 当前投入部分收益率
+	PerPart            float64           `json:"per_part"`              // 每份金额
+	ProfitAmount       float64           `json:"profit_amount"`         // 收益总额
+	Breeds             []*breed.BreedDto `json:"breeds,omitempty"`      // 账户绑定的品种
 }
--- module/account/model.go
+++ module/account/model.go
@@ -2,22 +2,24 @@ package account
 
 import (
 	"github.com/crazyhl/yzyx-materials/internal/model"
+	"github.com/crazyhl/yzyx-materials/module/breed"
 	"github.com/crazyhl/yzyx-materials/module/user"
 )
 
 // 记账账户
 type Account struct {
 	model.Model
-	Name               string    `gorm:"type:varchar(100);not null"`             // 账户名称
-	Description        string    `gorm:"type:varchar(255);default: '';not null"` // 账户描述，描述一下投资目的
-	UserId             uint      `gorm:"not null"`                               // 用户ID
-	User               user.User // 账户所属用户，外键
-	ExpectTotalMoney   float64   `gorm:"type:decimal(20,4);default: 0;not null"` // 预期投入总金额
-	ExpectRateOfReturn uint8     `gorm:"type:tinyint;default: 0;not null"`       // 预期收益率
-	RateOfReturn       float64   `gorm:"type:decimal(20,4);default: 0;not null"` // 实际收益率
-	TotalMoney         float64   `gorm:"type:decimal(20,4);default: 0;not null"` // 已经投入总金额
-	PerPartMoney       float64   `gorm:"type:decimal(20,4);default: 0;not null"` // 每份金额
-	ProfitAmount       float64   `gorm:"type:decimal(20,4);default: 0;not null"` // 收益总金额
+	Name               string         `gorm:"type:varchar(100);not null"`             // 账户名称
+	Description        string         `gorm:"type:varchar(255);default: '';not null"` // 账户描述，描述一下投资目的
+	UserId             uint           `gorm:"not null"`                               // 用户ID
+	User               user.User      // 账户所属用户，外键
+	ExpectTotalMoney   float64        `gorm:"type:decimal(20,4);default: 0;not null"` // 预期投入总金额
+	ExpectRateOfReturn uint8          `gorm:"type:tinyint;default: 0;not null"`       // 预期收益率
+	RateOfReturn       float64        `gorm:"type:decimal(20,4);default: 0;not null"` // 实际收益率
+	TotalMoney         float64        `gorm:"type:decimal(20,4);default: 0;not null"` // 已经投入总金额
+	PerPartMoney       float64        `gorm:"type:decimal(20,4);default: 0;not null"` // 每份金额
+	ProfitAmount       float64        `gorm:"type:decimal(20,4);default: 0;not null"` // 收益总金额
+	Breeds             []*breed.Breed `gorm:"many2many:account_breeds;"`
 }
 
 func (a Account) ToDto() *AccountDto {
